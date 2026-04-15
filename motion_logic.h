15ad5b703664687d7fc51c50d6ed56653fc504a3
Oleksandr Lozovyi <alexlozovoj1977@gmail.com>
2026-04-15 21:57:58 +0300

    Refactor MotionController for 16 DOF support

--- motion_logic.h
+++ motion_logic.h
@@ -3,31 +3,27 @@
 
 #include <stdint.h>
 
-// Кількість ступенів свободи (моторів/каналів)
-#define N_DOF 4 
+// Визначаємо кількість ступенів свободи (або каналів ЕЕГ)
+#define N_DOF 16 
 
 class MotionController {
 public:
     MotionController();
-
-    /**
-     * @brief Основний цикл оновлення (викликати в ISR або Task)
-     * @param target_phi Масив цільових фаз (0..65535)
-     * @param current_phi Масив поточних фаз від сенсорів
-     * @param output_torque Масив для запису розрахованих моментів
-     */
+    
+    // Головний метод оновлення стану
     void update(const uint16_t* target_phi, const uint16_t* current_phi, int16_t* output_torque);
-
-    // Налаштування Gain (Q8 формат)
+    
+    // Динамічна зміна коефіцієнтів
     void setGains(int16_t k_gain, int16_t damping_b);
 
 private:
-    int16_t sinLUT[1024];      // 10-bit Sine Look-Up Table
-    int16_t coupling_matrix[N_DOF][N_DOF];
-    int16_t K_gain;            // Коефіцієнт жорсткості
-    int16_t B_damping;         // Коефіцієнт демпфування
+    int16_t K_gain;
+    int16_t B_damping;
     
+    // LUT на 1024 елементи для економії пам'яті
+    int16_t sinLUT[1024];
+
     void initSinLUT();
 };
 
-#endif
+#endif // MOTION_LOGIC_H
